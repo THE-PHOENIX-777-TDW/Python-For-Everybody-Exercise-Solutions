0ad62b42867482187c217a90e406b09b4b6002ed
THE_PHOENIX_777_TDW <42874545+THE-PHOENIX-777-TDW@users.noreply.github.com>
2020-07-01 09:51:28 +0530

    Delete 2.5.py

--- Chapter 2 :- Variables, expressions, and statements/2.5.py
+++
@@ -1,4 +0,0 @@
-Celsius=float(input("Enter Temperature (In Celsius) : "))
-
-Fahrenheit=Celsius*1.8+32 #Formula for Celsius to farherenheit Conversion
-print("Temperature in Fahrenheit : ",Fahrenheit)
