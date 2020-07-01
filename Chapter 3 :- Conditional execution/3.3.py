a989eba70ec90bb403aba79363d371120ebc43d6
THE_PHOENIX_777_TDW <42874545+THE-PHOENIX-777-TDW@users.noreply.github.com>
2020-07-01 10:00:15 +0530

    Update 3.3.py

--- Chapter 3 :- Conditional execution/3.3.py
+++ Chapter 3 :- Conditional execution/3.3.py
@@ -1,20 +1,17 @@
-def computegrade(Grd):
-    if(Score >= 0.0 and Score <=1.0):
-        if(Score >= 0.9):
-            return("A")
-        elif(Score >= 0.8):
-            return("B")
-        elif(Score >= 0.7):
-            return("C")
-        elif(Score >= 0.6):
-            return("D")
-        else:
-            return("F")
-    else:
-        return("Bad score")
-
 try:
     Score=float(input("Enter score : "))
-    print(computegrade(Score))
+    if(Score >= 0.0 and Score <=1.0):
+        if(Score >= 0.9):
+            print("A")
+        elif(Score >= 0.8):
+            print("B")
+        elif(Score >= 0.7):
+            print("C")
+        elif(Score >= 0.6):
+            print("D")
+        else:
+            print("F")
+    else:
+        print("Bad score")
 except:
     print("Bad score")
